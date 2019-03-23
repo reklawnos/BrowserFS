8b4eaeca37160c9f27a3ffb9342de859760fbabe
Walker Henderson <wjahenderson@gmail.com>
2019-03-22 23:33:00 -0700

    some customizations for resonator

--- src/core/backends.ts
+++ src/core/backends.ts
@@ -1,13 +1,16 @@
 import {FileSystemConstructor, BFSCallback, FileSystem} from './file_system';
 import {ApiError} from './api_error';
 import {checkOptions} from './util';
+/*
 import AsyncMirror from '../backend/AsyncMirror';
 import Dropbox from '../backend/Dropbox';
 import Emscripten from '../backend/Emscripten';
 import FolderAdapter from '../backend/FolderAdapter';
 import HTML5FS from '../backend/HTML5FS';
 import InMemory from '../backend/InMemory';
+*/
 import IndexedDB from '../backend/IndexedDB';
+/*
 import LocalStorage from '../backend/LocalStorage';
 import MountableFileSystem from '../backend/MountableFileSystem';
 import OverlayFS from '../backend/OverlayFS';
@@ -15,9 +18,11 @@ import WorkerFS from '../backend/WorkerFS';
 import HTTPRequest from '../backend/HTTPRequest';
 import ZipFS from '../backend/ZipFS';
 import IsoFS from '../backend/IsoFS';
+*/
 
 // Monkey-patch `Create` functions to check options before file system initialization.
-[AsyncMirror, Dropbox, Emscripten, FolderAdapter, HTML5FS, InMemory, IndexedDB, IsoFS, LocalStorage, MountableFileSystem, OverlayFS, WorkerFS, HTTPRequest, ZipFS].forEach((fsType: FileSystemConstructor) => {
+// [AsyncMirror, Dropbox, Emscripten, FolderAdapter, HTML5FS, InMemory, IndexedDB, IsoFS, LocalStorage, MountableFileSystem, OverlayFS, WorkerFS, HTTPRequest, ZipFS]
+[IndexedDB].forEach((fsType: FileSystemConstructor) => {
   const create = fsType.Create;
   fsType.Create = function(opts?: any, cb?: BFSCallback<FileSystem>): void {
     const oneArg = typeof(opts) === "function";
@@ -39,7 +44,8 @@ import IsoFS from '../backend/IsoFS';
 /**
  * @hidden
  */
-const Backends = { AsyncMirror, Dropbox, Emscripten, FolderAdapter, HTML5FS, InMemory, IndexedDB, IsoFS, LocalStorage, MountableFileSystem, OverlayFS, WorkerFS, HTTPRequest, XmlHttpRequest: HTTPRequest, ZipFS };
+// const Backends = { AsyncMirror, Dropbox, Emscripten, FolderAdapter, HTML5FS, InMemory, IndexedDB, IsoFS, LocalStorage, MountableFileSystem, OverlayFS, WorkerFS, HTTPRequest, XmlHttpRequest: HTTPRequest, ZipFS };
+const Backends = { IndexedDB };
 // Make sure all backends cast to FileSystemConstructor (for type checking)
 const _: {[name: string]: FileSystemConstructor} = Backends;
 // tslint:disable-next-line:no-unused-expression
